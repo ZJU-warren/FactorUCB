7182879fbaeb18da34a07f24ed6ec40768072fcf
ZJU-warren <CS_wurui@163.com>
2020-01-15 15:19:31 +0800

    modify the result-img generator

--- Result.py
+++ Result.py
@@ -3,17 +3,19 @@ from Tools.DataTools import *
 import DataSetLink as DLSet
 
 
-def show_img(data_link):
-    df = load_data(data_link)[:5000]
-    print(df.columns)
-    x = df['n_round'].values
-    y = df['ratio'].values
-
+def show_img(result):
     plt.figure()
-    plt.plot(x, y)
+    for each in result.keys():
+        plt.plot(np.arange(result[each].shape[0]), result[each], label=each)
+    plt.title("Training Loss and Accuracy")
+    plt.xlabel("# round")
+    plt.ylabel("ratio")
+    plt.legend()
     plt.show()
 
 
 if __name__ == '__main__':
-    show_img(DLSet.result_link % 'LinUCB')
-    show_img(DLSet.result_link % 'hLinUCB')
+    result = {}
+    for each in ['LinUCB', 'hLinUCB', 'coLin', 'factorUCB']:
+        result[each] = load_data(DLSet.result_link % each)['ratio'][10000:].values
+    show_img(result)
